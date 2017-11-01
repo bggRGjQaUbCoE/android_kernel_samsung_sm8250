1a8bd829b530153ebf954606ae222962f2fca715
Vikram Mulukutla <markivx@codeaurora.org>
2017-11-01 12:11:03 -0700

    sched: fair: Work in isolation conditions into SMP placement path
    This snapshot is taken from msm-4.9 as of commit a81181397b9229d
    (Merge "msm: vidc: add P010 color format in govenors").
    
    Change-Id: Ifc22b1295d4d89c219f3b2cb7a238ef31865301d
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>
    [satyap@codeaurora.org: Port to 4.19 and fix merge conflicts]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7031,6 +7031,8 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 			return -1;
 		if (cpu_isolated(cpu))
 			continue;
+		if (cpu_isolated(cpu))
+			continue;
 		if (available_idle_cpu(cpu))
 			break;
 	}
