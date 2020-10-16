c9e4b01c8738f3ae3f598dd594ec3da2c3623c99
David Howells <dhowells@redhat.com>
2020-10-15 20:06:24 -0700

    mm/readahead: pass readahead_control to force_page_cache_ra
    Reimplement force_page_cache_readahead() as a wrapper around
    force_page_cache_ra().  Pass the existing readahead_control from
    page_cache_sync_readahead().
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Cc: Eric Biggers <ebiggers@google.com>
    Link: https://lkml.kernel.org/r/20200903140844.14194-7-willy@infradead.org
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2576,9 +2576,6 @@ void task_dirty_inc(struct task_struct *tsk);
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
-void force_page_cache_readahead(struct address_space *, struct file *,
-		pgoff_t index, unsigned long nr_to_read);
-
 void page_cache_sync_readahead(struct address_space *mapping,
 			       struct file_ra_state *ra,
 			       struct file *filp,
