44126e9f67b4fc5bac58c5efe12d15e612ceec46
Deepak Kumar <dkumar@codeaurora.org>
2020-11-09 15:12:31 +0530

    msm: kgsl: Check for gmu prealloc failure only in case of prealloc request
    Check for gmu prealloc failure only in case of prealloc request to avoid
    use of uninitialized ret variable.
    
    Change-Id: Ie0718f17fb9517e3e7659c277ff6f2fc3ef07f53
    Signed-off-by: Deepak Kumar <dkumar@codeaurora.org>
    Signed-off-by: Akhil P Oommen <akhilpo@codeaurora.org>

--- drivers/gpu/msm/adreno_a6xx_gmu.c
+++ drivers/gpu/msm/adreno_a6xx_gmu.c
@@ -1203,8 +1203,10 @@ static int a6xx_gmu_load_firmware(struct kgsl_device *device)
 		offset += sizeof(*blk);
 
 		if (blk->type == GMU_BLK_TYPE_PREALLOC_REQ ||
-				blk->type == GMU_BLK_TYPE_PREALLOC_PERSIST_REQ) {
+			blk->type == GMU_BLK_TYPE_PREALLOC_PERSIST_REQ) {
 			ret = gmu_prealloc_req(device, blk);
+
+			BUG_ON(ret);
 
 			if (ret)
 				return ret;
