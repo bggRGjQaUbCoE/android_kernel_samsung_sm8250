261668251dd9e5edca1ebd1cd962e03c8b5b2759
Quentin Perret <qperret@google.com>
2020-11-12 11:12:01 +0000

    BACKPORT: sched/fair: Fix overutilized update in enqueue_task_fair()
    [ Upstream commit 8e1ac4299a6e8726de42310d9c1379f188140c71 ]
    
    enqueue_task_fair() attempts to skip the overutilized update for new
    tasks as their util_avg is not accurate yet. However, the flag we check
    to do so is overwritten earlier on in the function, which makes the
    condition pretty much a nop.
    
    Fix this by saving the flag early on.
    
    Fixes: 2802bf3cd936 ("sched/fair: Add over-utilization/tipping point indicator")
    Reported-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Quentin Perret <qperret@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20201112111201.2081902-1-qperret@google.com
    Change-Id: I04a99c7db2d0559e838343762a928ac6caa1a9c4

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5732,6 +5732,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 	bool prefer_idle = sched_feat(EAS_PREFER_IDLE) ?
 				(schedtune_prefer_idle(p) > 0) : 0;
+	int task_new = !(flags & ENQUEUE_WAKEUP);
 	
 #ifdef CONFIG_PERF_MGR
 	unsigned long next_fps_boosted_util;
@@ -5739,6 +5740,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	int cur_group_id = -1, next_group_id = -1;
 	int alloc_cpu = -1;
 #endif
+
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
 	 * the cfs_rq utilization to select a frequency.
@@ -5854,7 +5856,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		 * overutilized. Hopefully the cpu util will be back to
 		 * normal before next overutilized check.
 		 */
-		if ((flags & ENQUEUE_WAKEUP) &&
+		if ((!task_new) &&
 		    !(prefer_idle && rq->nr_running == 1))
 			update_overutilized_status(rq);
 	}
