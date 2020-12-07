e36aaa98fcd724776046d336c799930ab8dc1aad
Yangtao Li <tiny.windzz@gmail.com>
2020-12-07 18:59:34 +0800

    f2fs: don't check PAGE_SIZE again in sanity_check_raw_super()
    Many flash devices read and write a single IO based on a multiple
    of 4KB, and we support only 4KB page cache size now.
    
    Since we already check page size in init_f2fs_fs(), so remove page
    size check in sanity_check_raw_super().
    
    Signed-off-by: Yangtao Li <tiny.windzz@gmail.com>
    Signed-off-by: Shaohua Liu <liush@allwinnertech.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3002,13 +3002,7 @@ static int sanity_check_raw_super(struct f2fs_sb_info *sbi,
 		return -EINVAL;
 	}
 
-	/* Currently, support only 4KB page cache size */
-	if (F2FS_BLKSIZE != PAGE_SIZE) {
-		f2fs_info(sbi, "Invalid page_cache_size (%lu), supports only 4KB",
-			  PAGE_SIZE);
-		return -EFSCORRUPTED;
-	}
-
+sanity_check_raw_super())
 	/* Currently, support only 4KB block size */
 	blocksize = 1 << le32_to_cpu(raw_super->log_blocksize);
 	if (blocksize != F2FS_BLKSIZE) {
