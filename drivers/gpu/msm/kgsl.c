c0d3f6353e38b6232b2083e9e860f33445017617
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-19 19:38:29 -0800

    msm: kgsl: Affine IRQ and worker kthread to the big CPU cluster
    These are in the critical path for rendering frames to the display, so
    mark them as performance-critical and affine them to the big CPU
    cluster. They aren't placed onto the prime cluster because the
    single-CPU prime cluster will be used to run the DRM IRQ and kthreads.
    DRM is more latency-critical than KGSL and we need to have DRM and KGSL
    running on separate CPUs for the best performance, so KGSL gets the big
    cluster.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5450,10 +5450,8 @@ int kgsl_request_irq(struct platform_device *pdev, const  char *name,
 	if (num < 0)
 		return num;
 
-	if (!strcmp(name, "kgsl_3d0_irq"))
-		irqflags |= IRQF_PERF_AFFINE;
-
-	ret = devm_request_irq(&pdev->dev, num, handler, irqflags, name, data);
+	ret = devm_request_irq(&pdev->dev, num, handler, IRQF_TRIGGER_HIGH |
+			       IRQF_PERF_AFFINE, name, data);
 
 	if (ret)
 		dev_err(&pdev->dev, "Unable to get interrupt %s: %d\n",
