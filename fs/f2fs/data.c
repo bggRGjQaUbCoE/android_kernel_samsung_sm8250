6bf9c768d557185e7bc3f8c90cc6c5e08e957472
Chao Yu <chao@kernel.org>
2022-12-21 20:13:45 +0800

    f2fs: fix to call clear_page_private_reference in .{release,invalid}_folio
    b763f3bedc2d ("f2fs: restructure f2fs page.private layout") missed
    to call clear_page_private_reference() in .{release,invalid}_folio,
    fix it, though it's not a big deal since folio_detach_private() was
    called to clear all privae info and reference count in the page.
    
    BTW, remove page_private_reference() definition as it never be used.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -4123,6 +4123,7 @@ void f2fs_invalidate_page(struct page *page, unsigned int offset,
 		}
 	}
 
+	clear_page_private_reference(page);
 	clear_page_private_gcing(page);
 
 	if (test_opt(sbi, COMPRESS_CACHE) &&
@@ -4146,6 +4147,7 @@ int f2fs_release_page(struct page *page, gfp_t wait)
 			clear_page_private_data(page);
 	}
 
+	clear_page_private_reference(page);
 	clear_page_private_gcing(page);
 
 	detach_page_private(page);
