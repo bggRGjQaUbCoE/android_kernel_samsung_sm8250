8ed3f790eeeb2a197f452db5c2a58722821d9ed7
bggRGjQaUbCoE <githubaccount56556@proton.me>
2023-04-16 11:15:27 +0800

    use clang 17

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
-config=vendor/lineage_y2qdcmw_defconfig
-export PATH="/home/van/clang12/bin:/home/van/GCC_4.9/aarch64-linux-android-4.9/bin:/home/van/GCC_4.9/arm-linux-androideabi-4.9/bin:$PATH"
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-android-
-export CROSS_COMPILE_ARM32=arm-linux-android-
+export CLANG_PATH=/home/van/clang17
+export PATH=${CLANG_PATH}/bin:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+export CLANG_PREBUILT_BIN=${CLANG_PATH}/bin
+export CC="ccache clang"
+export CXX="ccache clang++"
+export LD=ld.lld
+export LLVM=1
+export LLVM_IAS=1
+export ARCH=arm64
+export SUBARCH=arm64
 
-make -j12 CC=clang O=/home/van/Desktop/out $config
+make -j12 O=/home/van/Desktop/out vendor/lineage_y2qdcmw_defconfig
 
-# make -j16 CC=clang O=/home/van/Desktop/out
+# make -j12 CC=clang O=/home/van/Desktop/out
