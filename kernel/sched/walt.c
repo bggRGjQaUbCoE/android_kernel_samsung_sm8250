b89771a43cd16cd364f07f11926b2cf4c24bebaa
bggRGjQaUbCoE <githubaccount56556@proton.me>
2023-05-04 23:30:29 +0800

    update

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -2313,7 +2313,7 @@ static struct sched_cluster *alloc_new_cluster(const struct cpumask *cpus)
 
 	cluster = kzalloc(sizeof(struct sched_cluster), GFP_ATOMIC);
 	if (!cluster) {
-		__WARN_printf("Cluster allocation failed. Possible bad scheduling\n");
+		//__WARN_printf("Cluster allocation failed. Possible bad scheduling\n");
 		return NULL;
 	}
 
