bcffeff9a1e21502d25968f599fa06453a2f3b56
bggRGjQaUbCoE <githubaccount56556@proton.me>
2023-05-10 21:10:33 +0800

    mbcache: fix build

--- include/linux/mbcache.h
+++ include/linux/mbcache.h
@@ -32,6 +32,8 @@ struct mb_cache_entry {
 	atomic_t		e_refcnt;
 	/* Key in hash - stable during lifetime of the entry */
 	u32			e_key;
+	u32			e_referenced:1;
+	u32			e_reusable:1;
 	unsigned long		e_flags;
 	/* User provided value - stable during lifetime of the entry */
 	u64			e_value;
